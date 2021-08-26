c62539f56580ac4425ab2dcefa91417d30c1d56d
okawo80085 <okawo.198@gmail.com>
2021-08-26 11:40:23 +0300

    src: vrcommon: assert.h: Cleanup
    Added a valve copyright comment, clarified a few things, removed useless macro

--- src/vrcommon/assert.h
+++ src/vrcommon/assert.h
@@ -1,25 +1,29 @@
+//========= Copyright Valve Corporation ============//
+
 #pragma once
 
 #ifndef __OPENVR_ASSERT
 #define __OPENVR_ASSERT
 
 #include <iostream>
-// uncomment to disable assert()
+// uncomment to disable throw on assert
 // #define NDEBUG
 
-void OPENVR_ASSERT(bool x) {
+// not the best assert but it works
+void OPENVR_ASSERT(bool x, const char* msg) {
     if (!x) {
-       std::cout<<"Error!!Assert failed\n";
-       std::cout<<"on line"<<__LINE__<<"\n";
-       std::cout<<"in file"<<__FILE__<<"\n";
+       std::cout << "Assert failed, ";
+       std::cout << "line" << __LINE__ << ", ";
+       std::cout << "file '" << __FILE__ << "': ";
+       std::cout << msg << "\n";
+
+   #ifndef NDEBUG
        throw;
+   #endif
     }
 }
-#define assert(x) OPENVR_ASSERT(x)
 
-
- 
-// Use (void) to silent unused warnings.
-#define AssertMsg(exp, msg) (assert(exp))
+// macro wrapper to fix define errors
+#define AssertMsg(exp, msg) (OPENVR_ASSERT((exp), (msg)))
 
 #endif // #ifndef __OPENVR_ASSERT
