9a16204a9b0b2f0229ebdf6a0e73f0ec4da2c794
okawo <okawo.198@gmail.com>
2021-09-20 16:00:18 +0300

    src: vrcommon: Renamed assert.h -> vr_assert.h
    A better way to do an assert wrapper
    
    assert.h: Renamed into vr_assert.h
    ..: strtools_public.cpp: Accommodate for assert.h rename

--- src/vrcommon/strtools_public.cpp
+++ src/vrcommon/strtools_public.cpp
@@ -10,7 +10,7 @@
 #include <functional>
 #include <locale>
 #include <codecvt>
-#include <vrcommon/assert.h>
+#include <vrcommon/vr_assert.h>
 
 #if defined( _WIN32 )
 #include <windows.h>
--- src/vrcommon/vr_assert.h
+++ src/vrcommon/vr_assert.h
@@ -6,8 +6,7 @@
 #define OPENVR_ASSERT_LIB_H
 
 #include <iostream>
-// uncomment to disable throw on assert
-// #define NDEBUG
+#include <cassert>
 
 // not the best assert but it works
 inline void __OPENVR_ASSERT(bool x, const char* msg) {
@@ -16,18 +15,10 @@ inline void __OPENVR_ASSERT(bool x, const char* msg) {
        std::cout << "line" << __LINE__ << ", ";
        std::cout << "file '" << __FILE__ << "': ";
        std::cout << msg << "\n";
-
-#ifndef NDEBUG
-       throw;
-#endif
     }
-}
 
-// fix for <cassert> includes,
-// because cassert and assert.h includes will include this header
-// the proper way to fix this issue is to rename this file
-// to literally anything except assert.h, but for now this will do
-#define assert(exp) (__OPENVR_ASSERT((exp), ""))
+    assert(x);
+}
 
 // macro wrapper to fix define errors
 #define AssertMsg(exp, msg) (__OPENVR_ASSERT((exp), (msg)))
